7e8f1cdf2d76f078aca78a9b4b536d13494c2800
djmancia <biskerton07@gmail.com>
2021-08-13 18:51:34 -0500

    add axios and initial retreival of schedule

---
+++ schedules.js
@@ -0,0 +1,12 @@
+const axios = require('axios');
+
+const week = 2;
+const year = 2021;
+(async function getSchedule() {
+    try {
+        const data = await axios.get(`https://www.espn.com/nfl/schedule/_/year/${year}/week/${week}?xhr=1`);
+        console.log(data.data.content)
+    } catch (error) {
+        console.error(error);
+    }
+})()
